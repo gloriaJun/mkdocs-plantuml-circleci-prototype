6d3a273d937ff24b738ae5672cb8a146ceff3b97
gloriaJun <pureainu@gmail.com>
2023-12-15 13:08:00 +0900

    ci: modify circle ci config

--- start.sh
+++ start.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
-command=$1
+build_option=$1
 
-docker compose -f docker-compose.yml up -d plantuml-server
+function _exec() {
+  command=$1
+  message=$2
 
-if [ "${command}" == serve ]; then
-  mkdocs serve
-else
-  mkdocs build
+  echo ""
+  echo "o ${message}..."
+  echo " - command: ${command}"
+  eval ${command}
+}
+
+## run plantuml server
+_exec "docker compose -f docker-compose.yml up -d plantuml-server" "Run plantuml Server on Docker Container"
+
+if [ "${build_option}" == "build" ]; then
+  echo "hi"
 fi
+
+_exec "mkdocs ${build_option}" "Run mkdocs"
 
 # run
 #COMMAND=${command} docker compose -f docker-compose.yml run --rm=true mkdocs-builder
 
-docker compose stop plantuml-server
+_exec "docker compose stop plantuml-server" "Stop Docker Container"
 
 # clean images
-docker rmi `docker images --filter dangling=true -q` 2> /dev/null
+_exec "docker rmi `docker images --filter dangling=true -q` 2> /dev/null" "Clean dangling docker images"
